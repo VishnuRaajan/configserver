a052b8ef5847ea940d1229be719766656fa48e22
VishnuRaajan Rengarajan <vishnuraajanrengarajan@MacBookAir.lan>
2024-12-11 18:55:02 +0530

    added the yml files for microservices

--- accounts/src/main/java/com/bank/accounts/exception/ResourceNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.bank.accounts.exception;
-
-public class ResourceNotFoundException extends RuntimeException{
-
-    public ResourceNotFoundException(String resource,String field,String fieldValue) {
-
-        super(String.format("%s not found with %s : '%s'",resource,field,fieldValue));
-    }
-}
